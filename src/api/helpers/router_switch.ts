666ca1de5ba9c69c6d44df00dcd448611fe27eaf
TerThesz <60745997+TerThesz@users.noreply.github.com>
2021-08-17 17:40:26 +0200

    Update router_switch.ts

--- src/api/helpers/router_switch.ts
+++ src/api/helpers/router_switch.ts
@@ -2,11 +2,22 @@ import { Request, Response, NextFunction } from  'express';
 import { validationMiddleware } from '../middlewares';
 import { error } from '../helpers';
 
-export = (controller: { [key: string]: Function }, validation: { [key: string]: object } | undefined) => async (req: Request, res: Response, next: NextFunction) => {
-  const method = req.method.toLocaleLowerCase();
-  
-  if (!controller) return error(res, 405);
+export = (controller: { [key: string]: Function }, validation: { [key: string]: object }, ...middleware: [ Function ] | []) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    const method = req.method.toLocaleLowerCase();
+    
+    if (!controller) return error(res, 405);
 
-  if (validation) validationMiddleware(validation[method])(req, res, () => controller[method](req, res, next));
-  else controller[method](req, res, next);
+    if (validation && validation[method]) validationMiddleware(validation[method])(req, res, () => callController());
+    else callController();
+
+    function callController() {
+      let nextFunction = true;
+
+      middleware.forEach(mw => {
+        if (nextFunction) mw(req, res, () => nextFunction = false);
+      });
+
+      if (nextFunction) controller[method](req, res, next);
+    }
 }
