599454643e15fd0a150c8e549f2d71c83e0f1454
TerThesz <60745997+TerThesz@users.noreply.github.com>
2021-08-17 17:40:06 +0200

    Update validation_middleware.ts

--- src/api/middlewares/validation_middleware.ts
+++ src/api/middlewares/validation_middleware.ts
@@ -1,20 +1,36 @@
 import { Request, Response, NextFunction } from  'express';
 import { error } from '../helpers';
 
-export = (schema: any, validateBody: boolean = false) => async (req: Request, res: Response, next: Function) => {
-  const content = validateBody ? req.body : req.query;
-  
+export = (schema: any) => async (req: Request, res: Response, next: Function) => {
+  let errors = new Array<any>();
+  const content = req.method.toLocaleLowerCase() === 'get' ? req.query : req.body;
+
   try {
     await schema.validate(content, { abortEarly: false });
     next();
   } catch (err) {
     if (!err.errors?.length) throw err;
-    
-    err.errors.forEach((error: string) => {
-      const [ section, status, ...args ] = error.split('.');
-      if (!section || !status) err.errors[err.errors.indexOf(error)] = 'ce.0.' + error;
-    });
-    
-    error(res, 400, err.errors);
+    console.log(JSON.stringify(err, null, 2));
+
+    if (req.method.toLocaleLowerCase() === 'get') errors = err.errors;
+    else {
+      try {
+        await schema.validate(req.query, { abortEarly: false });
+        next();
+      } catch (_err) {
+        if (!req.query.length) errors = _err.inner;
+        else errors = err.inner;
+      }
+    }
+
+    if (errors.length) {
+      const additionalErrors = new Array<string>();
+
+      errors.forEach((error: any) => {
+        additionalErrors.push(`ce.0.${error.name}.${error.message}`);
+      });
+      
+      error(res, 400, additionalErrors);
+    }
   }
 }
