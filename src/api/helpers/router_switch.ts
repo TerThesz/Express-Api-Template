76f37dd88a5445141dee0f51a1275ebac9d014ab
TerThesz <60745997+TerThesz@users.noreply.github.com>
2021-08-17 17:05:19 +0200

    Update router_switch.ts

--- src/api/helpers/router_switch.ts
+++ src/api/helpers/router_switch.ts
@@ -2,12 +2,11 @@ import { Request, Response, NextFunction } from  'express';
 import { validationMiddleware } from '../middlewares';
 import { error } from '../helpers';
 
-export = (...switches: [ [ { [key: string]: Function }, { [key: string]: object } | undefined ] ]) => async (req: Request, res: Response, next: NextFunction) => {
+export = (controller: { [key: string]: Function }, validation: { [key: string]: Function } | undefined) => async (req: Request, res: Response, next: NextFunction) => {
   const method = req.method.toLocaleLowerCase();
-  const switchObject = switches.find(_switch => _switch[0][method]);
   
-  if (!switchObject) return error(res, 405);
+  if (!controller) return error(res, 405);
 
-  if (switchObject[1] && switchObject[1][method]) validationMiddleware(switchObject[1][method])(req, res, () => switchObject[0][method](req, res, next));
-  else switchObject[0][method](req, res, next);
+  if (validation) validationMiddleware(validation[method])(req, res, () => controller[method](req, res, next));
+  else controller[method](req, res, next);
 }
