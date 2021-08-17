11d5bbaf09c264ee4eb7c2e7102f1493b4d44081
TerThesz <60745997+TerThesz@users.noreply.github.com>
2021-08-17 15:22:04 +0200

    better switch

--- src/api/helpers/router_switch.ts
+++ src/api/helpers/router_switch.ts
@@ -2,10 +2,12 @@ import { Request, Response, NextFunction } from  'express';
 import { validationMiddleware } from '../middlewares';
 import { error } from '../helpers';
 
-export = (...switches: [ [ string, Function, any ] ]) => async (req: Request, res: Response, next: NextFunction) => {
-  const switchObject = switches.find(_switch => _switch[0] === req.method.toLocaleLowerCase());
+export = (...switches: [ [ { [key: string]: Function }, { [key: string]: Function } | undefined ] ]) => async (req: Request, res: Response, next: NextFunction) => {
+  const method = req.method.toLocaleLowerCase();
+  const switchObject = switches.find(_switch => _switch[0][method]);
   
   if (!switchObject) return error(res, 405);
 
-  validationMiddleware(switchObject[2])(req, res, () => switchObject[1](req, res, next));
+  if (switchObject[1] && switchObject[1][method]) validationMiddleware(switchObject[1][method])(req, res, () => switchObject[0][method](req, res, next));
+  else switchObject[0][method](req, res, next);
 }
